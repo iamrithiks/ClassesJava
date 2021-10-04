2fab0feb7a83686b5bc65e4f835f4fd9e0b95768
Rithik Samanthula <rithikdeveloper754@gmail.com>
2021-10-04 19:41:19 +0530

    Initial Commit

---
+++ src/code2rithik/github/io/Car.java
@@ -0,0 +1,27 @@
+package code2rithik.github.io;
+
+/**
+ * *
+ */
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if(validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
---
+++ src/code2rithik/github/io/Main.java
@@ -0,0 +1,11 @@
+package code2rithik.github.io;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car Holden = new Car();
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
+    }
+}
